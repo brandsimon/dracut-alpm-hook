6457bda1cd08b3442f60ae227b8bcc45027943ed
Simon Brand <simon.brand@postadigitale.de>
2019-11-27 23:27:17 +0100

    Initial version

---
+++ etc/pacman.d/scripts/dracut.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env dash
+CMDLINE="root=UUID=7af9b993-b17e-4443-92e1-9a7000c96b44"
+EFI_STUB="/usr/lib/systemd/boot/efi/linuxx64.efi.stub"
+update() {
+	local kver="${1}"
+	local pkgbasefile="/usr/lib/modules/${kver}/pkgbase"
+	local pkgname=""
+	pkgname="$(cat "${pkgbasefile}")"
+	local destfile="/boot/EFI/Arch/${pkgname}.efi"
+	if test -f "${destfile}"; then
+		echo "Move ${destfile} to ${destfile}.old"
+		mv "${destfile}" "${destfile}.old"
+	fi
+	echo "Update ${pkgname}: ${kver}"
+	if ! dracut --kver "${kver}" --uefi --uefi-stub "${EFI_STUB}" --nolvmconf --nomdadmconf --hostonly-cmdline --kernel-cmdline "${CMDLINE}" "${destfile}" > /dev/null; then
+		exit 1
+	fi
+	return 0
+}
+file_to_kver() {
+	local file="${1}"
+	local kver=""
+	kver="$(echo "${file}" | sed -e "s/usr\/lib\/modules\/\([^/]*\)\/pkgbase/\1/g")"
+	if test -f "/usr/lib/modules/${kver}/pkgbase"; then
+		echo "${kver}"
+	fi
+}
+kvers=""
+all_kvers=""
+read_something=""
+while read -r line; do
+	read_something="yes"
+	kver="$(file_to_kver "${line}")"
+	if test -z "${kver}"; then
+		all_kvers="yes"
+	else
+		kvers="${kvers} ${kver}"
+	fi
+done
+if test -z "${read_something}"; then
+	all_kvers="yes"
+fi
+if ! test -z "${all_kvers}"; then
+	echo "Update all kernels"
+	cd /
+	for line in usr/lib/modules/*/pkgbase; do
+		kver="$(file_to_kver "${line}")"
+		update "${kver}"
+	done
+else
+	for kver in ${kvers}; do
+		update "${kver}"
+	done
+fi
